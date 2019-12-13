237e121294c6ffc38bc2cb081a8e7949307d4397
Sasha Mansurov <sasha@mansurov.me>
2019-12-13 20:16:40 +0300

    day 13 part 2

--- day13/main.go
+++ day13/main.go
@@ -28,8 +28,51 @@ func main() {
 		input = append(input, n)
 	}
 
-	fmt.Println("part one:", computer(input)) // 180
+	fmt.Println("part two:", computer(input)) // 8777
 }
+
+func draw(input map[point]int) {
+	maxRows := 0
+	for point := range input {
+		if point.y > maxRows {
+			maxRows = point.y
+		}
+	}
+	length := maxRows + 1
+	rows := make([]map[int]string, length)
+	for k, v := range input {
+		tile := ""
+		switch v {
+		case 0:
+			tile = " "
+		case 1:
+			tile = "#"
+		case 2:
+			tile = "X"
+		case 3:
+			tile = "-"
+		case 4:
+			tile = "O"
+		}
+		if rows[k.y] == nil {
+			rows[k.y] = make(map[int]string)
+		}
+		rows[k.y][k.x] = tile
+	}
+	output := make([]string, length)
+	for key, row := range rows {
+		out := ""
+		for i := 0; i < len(row); i++ {
+			value := row[i]
+			out += value
+		}
+		output[key] = out
+	}
+	// for _, v := range output {
+	// 	fmt.Println(v)
+	// }
+}
+
 func getIndex(input []int, index int, mode int, relativeBase int) int {
 	// 0 == position mode
 	// 1 == immediate mode
@@ -47,13 +90,16 @@ func computer(array []int) int {
 	index := 0
 	commands := make([]int, math.MaxInt16)
 	copy(commands, array)
+	commands[0] = 2 // free mode
 	output := 0
 	relativeBase := 0
 	outputCounter := 0
+	score := 0
+	var paddle point
+	var ball point
 	x := 0
 	y := 0
 	all := make(map[point]int)
-	blockCounter := 0
 
 Loop:
 	for {
@@ -85,8 +131,16 @@ Loop:
 		case 3:
 			mode1 := params % 10
 			result := 0
-			fmt.Printf("Enter ID of the system: ")
-			fmt.Scanln(&result)
+			draw(all)
+			if paddle.x < ball.x {
+				result = 1
+			} else if paddle.x > ball.x {
+				result = -1
+			} else {
+				result = 0
+			}
+			// fmt.Printf("Enter ID of the system: ")
+			// fmt.Scanln(&result)
 			commands[getIndex(commands, index+1, mode1, relativeBase)] = result
 			// fmt.Println("INPUT:", result)
 			index += 2
@@ -99,9 +153,16 @@ Loop:
 			} else if outputCounter%3 == 1 {
 				y = output
 			} else {
-				all[point{x, y}] = output
-				if output == 2 {
-					blockCounter++
+				if x == -1 && y == 0 {
+					score = output
+				} else {
+					all[point{x, y}] = output
+					if output == 3 {
+						paddle = point{x, y}
+					}
+					if output == 4 {
+						ball = point{x, y}
+					}
 				}
 			}
 			outputCounter++
@@ -153,12 +214,12 @@ Loop:
 			relativeBase += commands[getIndex(commands, index+1, mode1, relativeBase)]
 			index += 2
 		case 99:
-			// fmt.Println("HALT", all, blockCounter)
+			// fmt.Println("HALT")
 			break Loop
 		default:
 			// fmt.Println("WRONG", opcode)
 			break Loop
 		}
 	}
-	return blockCounter
+	return score
 }
